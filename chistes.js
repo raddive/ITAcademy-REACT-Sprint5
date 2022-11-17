3276544c8eea882f03637704fadfe2426a323651
raddive <alvarogarcia8@hotmail.com>
2022-11-17 11:55:50 +0100

    N1.E3

--- chistes.js
+++ chistes.js
@@ -1,3 +1,35 @@
+var reportAcudits = [];
+var sActualJoke;
+function JokeExist() {
+    var jokeExist = reportAcudits.filter(function (item) { return item.joke == sActualJoke; });
+    if (jokeExist.length === 0)
+        return false;
+    else
+        return true;
+}
+function JokeRated() {
+    var jokeRated = reportAcudits.filter(function (item) { return item.joke == sActualJoke && item.score > 0; });
+    if (jokeRated.length === 0)
+        return false;
+    else
+        return true;
+}
+function ButtonStatus() {
+    var element = document.getElementById('currentJoke');
+    if (element && element.innerText != "") {
+        document.getElementById('score1_btn').classList.remove('disabled');
+        document.getElementById('score2_btn').classList.remove('disabled');
+        document.getElementById('score3_btn').classList.remove('disabled');
+        if (JokeRated()) {
+            document.getElementById('next_btn').classList.remove('disabled');
+            document.getElementById('score1_btn').classList.add('disabled');
+            document.getElementById('score2_btn').classList.add('disabled');
+            document.getElementById('score3_btn').classList.add('disabled');
+        }
+        else
+            document.getElementById('next_btn').classList.add('disabled');
+    }
+}
 //Implementación con FETCH y PROMISES
 function NextJoke() {
     fetch('https://icanhazdadjoke.com/', {
@@ -7,6 +39,19 @@ function NextJoke() {
         .then(function (response) { return response.json(); })
         .then(function (json) {
         document.getElementById("currentJoke").innerText = json.joke;
+        sActualJoke = json.joke;
         // console.log(json);
-    })["catch"](function () { console.log("Error en la llamada a la API"); });
+    })
+        .then(function () { return ButtonStatus(); })["catch"](function () { console.log("Error en la llamada a la API"); });
+}
+function Score(iScore) {
+    // console.log("La puntuación ha sido " + iScore);
+    sActualJoke = document.getElementById("currentJoke").innerText;
+    var d = new Date();
+    var sDate = d.toISOString();
+    if (sActualJoke != "") {
+        reportAcudits.push({ joke: sActualJoke, score: iScore, date: sDate });
+        ButtonStatus();
+        console.log(reportAcudits);
+    }
 }
--- chistes.ts
+++ chistes.ts
@@ -1,4 +1,49 @@
 
+let reportAcudits:{
+    joke: string;
+    score: number;
+    date: string;
+    }[ ] = [];
+
+let sActualJoke:string;
+
+function JokeExist():boolean{
+
+    let jokeExist=reportAcudits.filter( (item) => item.joke == sActualJoke );
+    if(jokeExist.length===0)
+        return false;
+    else
+        return true;
+}   
+
+function JokeRated():boolean{
+
+    let jokeRated=reportAcudits.filter( (item) => item.joke == sActualJoke && item.score>0);
+    if(jokeRated.length===0)
+        return false;
+    else
+        return true;
+}
+
+function ButtonStatus(){
+    var element = document.getElementById('currentJoke');
+    if(element && element.innerText!="")
+    {
+        document.getElementById('score1_btn').classList.remove('disabled');
+        document.getElementById('score2_btn').classList.remove('disabled');
+        document.getElementById('score3_btn').classList.remove('disabled');
+        if(JokeRated())
+        {
+            document.getElementById('next_btn').classList.remove('disabled');
+            document.getElementById('score1_btn').classList.add('disabled');
+            document.getElementById('score2_btn').classList.add('disabled');
+            document.getElementById('score3_btn').classList.add('disabled');
+        }
+        else
+            document.getElementById('next_btn').classList.add('disabled');
+    }
+}
+
 //Implementación con FETCH y PROMISES
 function NextJoke():void {
 
@@ -8,7 +53,22 @@ function NextJoke():void {
         .then(response => response.json())
         .then(json => {
                         document.getElementById("currentJoke").innerText = json.joke;
+                        sActualJoke=json.joke;
                         // console.log(json);
                     })
+        .then( () => ButtonStatus())
         .catch( () => {console.log("Error en la llamada a la API")});
+}
+
+function Score(iScore:number) {
+    // console.log("La puntuación ha sido " + iScore);
+    sActualJoke=document.getElementById("currentJoke").innerText;
+    const d:Date = new Date();
+    let sDate = d.toISOString();
+    if(sActualJoke != "")
+    {
+        reportAcudits.push({joke: sActualJoke, score: iScore ,date: sDate});
+        ButtonStatus();
+        console.log(reportAcudits);
+    }
 }
